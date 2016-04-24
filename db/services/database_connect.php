44a38dd2166b582c2700e33302cdc9860c07474a
loremarsy <lore_marsy_blackeyes@yahoo.com>
2016-04-24 11:35:52 +0300

    modified data from php script to extrat data and json encode

---
+++ db/services/database_connect.php
@@ -0,0 +1,17 @@
+<?php
+
+set_time_limit(0);
+error_reporting(E_ALL);
+
+ $DataBaseAddress = "localhost";
+ $DataBaseUser = "root";
+ $DataBasePassword ="";
+ $DataBaseName = "airpllutiondiseases";
+
+ $connexion = mysql_connect($DataBaseAddress,$DataBaseUser,$DataBasePassword) or die("could not connect to the database");
+ 
+ 
+ 
+ mysql_select_db($DataBaseName, $connexion) or die("cannot find the database!");
+ 
+?>
